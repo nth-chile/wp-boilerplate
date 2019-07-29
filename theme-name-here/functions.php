455423c5524cf09ce559dd9e0acc1f561749aacb
Jared Salzano <jaredsalzano@gmail.com>
2019-07-29 16:08:47 +0700

    Modify functions.php, package.json; remove style.css; add es6/

--- theme-name-here/functions.php
+++ theme-name-here/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function enqueue_scripts_and_styles() {
-  wp_enqueue_style("theme-style", get_stylesheet_directory_uri(), array(), "1");
+  wp_enqueue_style("theme-style", get_stylesheet_directory_uri() . '/style.css', array(), "1");
+  wp_enqueue_script("theme-script", get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), "1");
 }
 
 add_action( "wp_enqueue_scripts", "enqueue_scripts_and_styles" );
